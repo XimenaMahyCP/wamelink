46ecae947dcaba3987931d9246a7de2de854669f
XimenaMahyCP <skylinestorear@gmail.com>
2025-08-07 15:51:22 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Actualización automática del año en el footer
-    const yearSpan = document.getElementById('current-year');
-    if (yearSpan) {
-        // Establece el año actual basado en la fecha del sistema del visitante.
-        // Se ha corregido para que no muestre un año fijo.
-        yearSpan.textContent = new Date().getFullYear();
-    }
-});
